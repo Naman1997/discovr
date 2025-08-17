b66abb2f2ac51a1f6396914476631a9048cae71e
Naman Arora <aroranaman17@gmail.com>
2025-08-17 16:13:01 +1000

    SCRUM-7: Implement basic passive scanning (#6)
    * Initial version for passive scanning. Needs code cleanup
    
    * Delete test code
    
    * Listen on all interfaces by default
    
    * Use a ticker to manually close the pcap handler
    
    * Parameterize scan duration
    
    * Rename file and make changes to the printPacketInfo function
    
    * Add todo
    
    * chore: go fmt auto-format
    
    * Remove unused imports
    
    ---------
    
    Co-authored-by: Naman1997 <19908560+Naman1997@users.noreply.github.com>

--- cmd/passive.go
+++ cmd/passive.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/Naman1997/discovr/internal"
 	"github.com/spf13/cobra"
 )
 
-var Interface string
+var (
+	Interface string
+	ScanTime  int
+)
 
 var passiveCmd = &cobra.Command{
 	Use:   "passive",
 	Short: "Scan local network passively",
 	Long:  `Reads incomming packets to determine devices present on the network`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("passive called")
-		internal.PassiveScan(Interface)
+		internal.PassiveScan(Interface, ScanTime)
 	},
 }
 
 func init() {
 	localCmd.AddCommand(passiveCmd)
-
-	passiveCmd.Flags().StringVarP(&Interface, "interface", "i", "eth0", "Interface to read packets from")
+	passiveCmd.Flags().StringVarP(&Interface, "interface", "i", "any", "Interface to read packets from")
+	passiveCmd.Flags().IntVarP(&ScanTime, "duration", "d", 10, "Number of seconds to run the scan")
 }
