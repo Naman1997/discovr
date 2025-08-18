f9cadb5794326fdafff8657543b4e0627dba66fc
Naman1997 <aroranaman17@gmail.com>
2025-08-18 19:44:03 +1000

    Remove unused function

--- internal/active.go
+++ internal/active.go
@@ -92,29 +92,6 @@ func createScanner(targets string, ports string, nmapPath string) (*nmap.Scanner
 	}
 }
 
-func countByOS(result *nmap.Run) {
-	var (
-		linux, windows int
-	)
-
-	// Count the number of each OS for all hosts.
-	for _, host := range result.Hosts {
-		for _, match := range host.OS.Matches {
-			for _, class := range match.Classes {
-				switch class.OSFamily() {
-				case osfamily.Linux:
-					linux++
-				case osfamily.Windows:
-					windows++
-				}
-			}
-
-		}
-	}
-
-	fmt.Printf("Discovered %d linux hosts and %d windows hosts out of %d total up hosts.\n", linux, windows, result.Stats.Hosts.Up)
-}
-
 func extractNmap() (string, string) {
 	nmapVersion := getEnvVariable("NMAP_VERSION")
 	nmapBinaryName := "nmap"
