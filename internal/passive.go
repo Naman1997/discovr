822c4e2176b4edde5f2e930739871baf2c903820
Naman1997 <aroranaman17@gmail.com>
2025-08-19 23:28:49 +1000

    Fix CI

--- internal/passive.go
+++ internal/passive.go
@@ -31,7 +31,7 @@ func PassiveScan(device string, scanSeconds int) {
 	err := sem.Acquire(ctx, 2)
 	if err != nil {
 		fmt.Println("")
-		fmt.Printf("Exiting! Reason:", err)
+		fmt.Print("Exiting! Reason:", err)
 		return
 	}
 }
