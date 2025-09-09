74ea875d4d58854347079e4dc9f24b9236d92d2a
Oemar Burhan <116046925+Viru-Vi@users.noreply.github.com>
2025-09-09 12:44:45 +1000

    SCRUM-136: Updated CLI calling mechanism for Active, Nmap, and Passive scans (#32)
    * improve the cmd flags for active and nmap
    
    * delete cmd/local.go file
    
    * updated passive CLI commands
    
    * SCRUM-136: improved the CLI commands for active, nmap and passive scans
    
    * SCRUM-136: improved the CLI commands for active, nmap and passive scans

--- cmd/active.go
+++ cmd/active.go
@@ -6,11 +6,7 @@ import (
 )
 
 var (
-	Target      string
-	Ports       string
-	OsDetection bool
-	PathActive  string
-	NmapScan    bool
+	ExportPathActive string
 )
 
 var activeCmd = &cobra.Command{
@@ -18,26 +14,13 @@ var activeCmd = &cobra.Command{
 	Short: "Scan local network actively",
 	Long:  `Sends network requests across the CIDR range to determine device ip, mac address and other details`,
 	Run: func(cmd *cobra.Command, args []string) {
-		header := activeScan(Target, Ports, OsDetection, NmapScan)
-		internal.ActiveExport(PathActive, header, NmapScan)
+		internal.DefaultScan()
+		header := []string{"Interface", "Dest_IP", "Dest_Mac"}
+		internal.ActiveExport(ExportPathActive, header, false)
 	},
 }
 
 func init() {
-	localCmd.AddCommand(activeCmd)
-	activeCmd.Flags().StringVarP(&Target, "target", "t", "127.0.0.1", "Target CIDR range or IP address to scan")
-	activeCmd.Flags().StringVarP(&Ports, "ports", "p", "", "Ports to scan on target systems (defaults to top 1000 most common ports)")
-	activeCmd.Flags().BoolVarP(&OsDetection, "detect-os", "d", false, "Enable OS detection (requires sudo)")
-	activeCmd.Flags().StringVarP(&PathActive, "export", "e", "", "Export results to CSV file")
-	activeCmd.Flags().BoolVarP(&NmapScan, "nmapscan", "n", false, "Use nmap for scanning (default false)")
-}
-
-func activeScan(targets string, ports string, osDetection bool, nmapScan bool) []string {
-	if nmapScan {
-		internal.NmapScan(targets, ports, osDetection)
-		return []string{"Port", "Protocol", "State", "Service", "Product"}
-	} else {
-		internal.DefaultScan()
-		return []string{"Interface", "Dest_IP", "Dest_Mac"}
-	}
+	rootCmd.AddCommand(activeCmd)
+	activeCmd.Flags().StringVarP(&ExportPathActive, "export", "e", "", "Export results to CSV file")
 }
---
+++ cmd/nmap.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/Naman1997/discovr/internal"
+	"github.com/spf13/cobra"
+)
+
+var (
+	Target      string
+	Ports       string
+	OsDetection bool
+	PathActive  string
+)
+
+var nmapCmd = &cobra.Command{
+	Use:   "nmap",
+	Short: "Scan network with nmap",
+	Long:  `Sends network requests with NMAP tool across the CIDR range to determine device ip, mac address and other details`,
+	Run: func(cmd *cobra.Command, args []string) {
+		internal.NmapScan(Target, Ports, OsDetection)
+		header := []string{"Port", "Protocol", "State", "Service", "Product"}
+		internal.ActiveExport(PathActive, header, true)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(nmapCmd)
+	nmapCmd.Flags().StringVarP(&Target, "target", "t", "127.0.0.1", "Target CIDR range or IP address to scan")
+	nmapCmd.Flags().StringVarP(&Ports, "ports", "p", "", "Ports to scan on target systems (defaults to top 1000 most common ports)")
+	nmapCmd.Flags().BoolVarP(&OsDetection, "detect-os", "d", false, "Enable OS detection (requires sudo)")
+	nmapCmd.Flags().StringVarP(&PathActive, "export", "e", "", "Export results to CSV file")
+}
