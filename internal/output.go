94867fc097cb227a350b6c3ae2e3e1f9e5ad366a
Muhammad-Sohaib093 <muhammadsohaib093@gmail.com>
2025-09-15 04:29:19 -0500

    SCRUM-124: Moved CSV Headers to output.go (#39)
    * SCRUM-124: Add mod tidy to github action
    
    * chore: go auto-format
    
    * SCRUM-124: Moved all csv headers to output.go
    
    ---------
    
    Co-authored-by: Muhammad-Sohaib093 <224501124+Muhammad-Sohaib093@users.noreply.github.com>

--- internal/output.go
+++ internal/output.go
@@ -10,7 +10,7 @@ import (
 func Export(filePath string, header []string, rows [][]string) error {
 	if filepath.Ext(filePath) != ".csv" {
 		filePath = filePath + ".csv"
-		fmt.Println("Export path did not have .csv extension, saving as:", filePath)
+		fmt.Println("\nExport path did not have .csv extension, saving as:", filePath)
 	}
 
 	// Check if file exists
@@ -40,12 +40,13 @@ func Export(filePath string, header []string, rows [][]string) error {
 			return fmt.Errorf("error writing row: %v", err)
 		}
 	}
-
+	fmt.Printf("Saving to: %v", filePath)
 	return nil
 }
 
 // Convert Passive results
-func PassiveExport(path string, header []string) {
+func PassiveExport(path string) {
+	header := []string{"Source_IP", "Protocol", "Source_MAC", "Destination_Mac", "Ethernet_Type"}
 	if path == "" {
 		return
 	}
@@ -57,12 +58,13 @@ func PassiveExport(path string, header []string) {
 }
 
 // Convert Active results
-func ActiveExport(path string, header []string, nmapScan bool) {
+func ActiveExport(path string, nmapScan bool) {
 	if path == "" {
 		return
 	}
 	//Changes for scrum-136
 	if nmapScan {
+		header := []string{"Port", "Protocol", "State", "Service", "Product"}
 		rows := make([][]string, len(active_results))
 		for i, r := range active_results {
 			rows[i] = []string{r.Port, r.Protocol, r.State, r.Service, r.Product}
@@ -70,6 +72,7 @@ func ActiveExport(path string, header []string, nmapScan bool) {
 		Export(path, header, rows)
 	} else {
 		// for default scan
+		header := []string{"Interface", "Dest_IP", "Dest_Mac"}
 		rows := make([][]string, len(defaultscan_results))
 		for i, r := range defaultscan_results {
 			rows[i] = []string{r.Interface, r.Dest_IP, r.Dest_Mac}
@@ -80,7 +83,8 @@ func ActiveExport(path string, header []string, nmapScan bool) {
 }
 
 // Convert AWS results
-func AwsExport(path string, header []string) {
+func AwsExport(path string) {
+	header := []string{"InstanceId", "PublicIp", "PrivateIPs", "MacAddress", "VpcId", "SubnetId", "Hostname", "Region"}
 	if path == "" {
 		return
 	}
