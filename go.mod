c51e35f7ff9c5823f78b0fbe4d3dbe89c7978ba4
Naman Arora <aroranaman17@gmail.com>
2025-09-28 06:48:35 +1000

    DB-2: Add GCP scanner (#43)
    * Initial version for GCP scanner
    
    * Get OS type as well
    
    * Add export functionality
    
    * chore: go auto-format
    
    ---------
    
    Co-authored-by: Naman1997 <19908560+Naman1997@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -14,10 +14,14 @@ require (
 	github.com/charmbracelet/huh v0.7.0
 	github.com/google/gopacket v1.1.19
 	github.com/spf13/cobra v1.9.1
-	golang.org/x/sync v0.16.0
+	golang.org/x/sync v0.17.0
+	google.golang.org/api v0.250.0
 )
 
 require (
+	cloud.google.com/go/auth v0.16.5 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.8.4 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.11.2 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal v1.1.2 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.1.1 // indirect
@@ -46,8 +50,14 @@ require (
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -62,8 +72,17 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/crypto v0.41.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/oauth2 v0.31.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250908214217-97024824d090 // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
 )
---
+++ internal/gcp.go
@@ -0,0 +1,200 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"google.golang.org/api/cloudresourcemanager/v1"
+	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+)
+
+var gcp_results []GcpScanResult
+
+type GcpScanResult struct {
+	ProjectId     string
+	InstanceId    string
+	InstanceName  string
+	Hostname      string
+	OsType        string
+	Zone          string
+	InterfaceName string
+	InternalIP    string
+	ExternalIPs   string
+	VPC           string
+	Subnet        string
+}
+
+func GcpScan(credFile string, projectFilterStr string) {
+	ctx := context.Background()
+	var resourceManagerClient *cloudresourcemanager.Service
+	var computeService *compute.Service
+	var err error
+
+	// Create clients based on auth type
+	if credFile != "" {
+		resourceManagerClient, err = cloudresourcemanager.NewService(ctx, option.WithCredentialsFile(credFile))
+		if err != nil {
+			log.Fatalf("Failed to create resource manager client: %v", err)
+		}
+
+		computeService, err = compute.NewService(ctx, option.WithCredentialsFile(credFile))
+		if err != nil {
+			log.Fatalf("Failed to create compute service: %v", err)
+		}
+	} else {
+		resourceManagerClient, err = cloudresourcemanager.NewService(ctx)
+		if err != nil {
+			log.Fatalf("Failed to create resource manager client: %v", err)
+		}
+
+		computeService, err = compute.NewService(ctx)
+		if err != nil {
+			log.Fatalf("Failed to create compute service: %v", err)
+		}
+	}
+
+	// List All Projects
+	projects, err := resourceManagerClient.Projects.List().Do()
+	if err != nil {
+		log.Fatalf("Failed to list projects: %v", err)
+	}
+
+	// Process Projects
+	if len(projects.Projects) == 0 {
+		fmt.Println("No projects found.")
+		return
+	}
+
+	// Convert project filter into a list
+	filteredProjects := strings.Split(projectFilterStr, ",")
+
+	for _, project := range projects.Projects {
+		if contains(filteredProjects, project.Name) || projectFilterStr == "" {
+			fmt.Printf("Checking instances for project: %s\n", project.Name)
+
+			listInstanceNetworkInfo(computeService, project.ProjectId)
+			// TODO: Enable on debug
+			// if err != nil {
+			//     log.Printf("Error retrieving instance network info for project %s: %v", project.ProjectId, err)
+			// }
+			fmt.Println()
+		}
+	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+// listInstanceNetworkInfo retrieves network details for instances in a specific project
+func listInstanceNetworkInfo(computeService *compute.Service, projectID string) error {
+
+	// TODO: Figure out pagination
+	instanceList, err := computeService.Instances.AggregatedList(projectID).Do()
+	if err != nil {
+		return fmt.Errorf("failed to list instances: %v", err)
+	}
+
+	// Process instances from all zones
+	for zoneName, instancesScopedList := range instanceList.Items {
+		zone := strings.TrimPrefix(zoneName, "zones/")
+
+		// Skip zones with no instances
+		if len(instancesScopedList.Instances) == 0 {
+			continue
+		}
+
+		for _, instance := range instancesScopedList.Instances {
+			for _, networkInterface := range instance.NetworkInterfaces {
+
+				// Print item
+				fmt.Println()
+				fmt.Printf("Project ID: %s\n", projectID)
+				fmt.Printf("Instance ID: %d\n", instance.Id)
+				fmt.Printf("Instance Name: %s\n", instance.Name)
+				fmt.Printf("Hostname: %s\n", instance.Hostname)
+
+				// Get OS details
+				var osType string
+				for _, disk := range instance.Disks {
+					if disk.Boot {
+						if disk.Licenses != nil && len(disk.Licenses) > 0 {
+							// Split the URL and get the last part
+							parts := strings.Split(disk.Licenses[0], "/")
+							if len(parts) > 0 {
+								osType = parts[len(parts)-1]
+								fmt.Printf("OS: %s\n", osType)
+								break
+							}
+						}
+					}
+				}
+
+				fmt.Printf("Zone: %s\n", zone)
+				fmt.Printf("Interface Name: %s\n", networkInterface.Name)
+
+				// Internal IP
+				if networkInterface.NetworkIP != "" {
+					fmt.Printf("Internal IP: %s\n", networkInterface.NetworkIP)
+				}
+
+				// Collect all NatIP values (these are external Ips)
+				var natIPs []string
+				var natIPString string
+				for _, accessConfig := range networkInterface.AccessConfigs {
+					natIPs = append(natIPs, accessConfig.NatIP)
+				}
+				if len(natIPs) > 0 {
+					natIPString = fmt.Sprintf("[%s]", strings.Join(natIPs, ","))
+					fmt.Printf("External IPs: %s\n", natIPString)
+				}
+
+				// VPC
+				var vpcID string
+				if networkInterface.Network != "" {
+					networkParts := strings.Split(networkInterface.Network, "/")
+					if len(networkParts) > 0 {
+						vpcID = networkParts[len(networkParts)-1]
+						fmt.Printf("VPC: %s\n", vpcID)
+					}
+				}
+
+				// Subnet
+				var subnetID string
+				if networkInterface.Subnetwork != "" {
+					subnetParts := strings.Split(networkInterface.Subnetwork, "/")
+					if len(subnetParts) > 0 {
+						subnetID = subnetParts[len(subnetParts)-1]
+						fmt.Printf("Subnet: %s\n", subnetID)
+					}
+				}
+
+				// Collect results
+				result := GcpScanResult{
+					ProjectId:     projectID,
+					InstanceId:    strconv.FormatUint(instance.Id, 10),
+					InstanceName:  instance.Name,
+					Hostname:      instance.Hostname,
+					OsType:        osType,
+					Zone:          zone,
+					InterfaceName: networkInterface.Name,
+					InternalIP:    networkInterface.NetworkIP,
+					ExternalIPs:   natIPString,
+					VPC:           vpcID,
+					Subnet:        subnetID,
+				}
+				gcp_results = append(gcp_results, result)
+			}
+		}
+	}
+	return nil
+}
