267924a90d30985f98007c224123f78dba7733a1
Viru-Vi <oemar.edu@gmail.com>
2025-09-30 23:02:03 +0930

    DB-9.2: adding shorthand for  count and timeout

--- cmd/active.go
+++ cmd/active.go
@@ -34,8 +34,8 @@ func init() {
 	activeCmd.Flags().StringVarP(&targetCIDR, "cidr", "r", "", "Target CIDR to scan (default: interface network)")
 	activeCmd.Flags().StringVarP(&ExportPathActive, "export", "e", "", "Export results to CSV file")
 	activeCmd.Flags().IntVarP(&concurrency, "concurrency", "p", 50, "Number of concurrent workers (default 50)")
-	activeCmd.Flags().IntVarP(&timeout, "timeout", "", 2, "Timeout in seconds to wait for each reply (default 2)")
-	activeCmd.Flags().IntVarP(&count, "count", "", 1, "Number of requests to send to each IP (default 1)")
+	activeCmd.Flags().IntVarP(&timeout, "timeout", "t", 2, "Timeout in seconds to wait for each reply (default 2)")
+	activeCmd.Flags().IntVarP(&count, "count", "c", 1, "Number of requests to send to each IP (default 1)")
 	err := activeCmd.MarkFlagRequired("interface")
 	if err != nil {
 		fmt.Println("Error marking flag as required:", err)
